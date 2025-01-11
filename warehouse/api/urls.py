72b9967e993e1ac3fedf6a726f07b4b2b3cac11d
Ktulha <9145140@gmail.com>
2025-01-11 23:20:45 +0300

    readme

--- warehouse/api/urls.py
+++ warehouse/api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from api.views import ProductModelViewSet, TransactionCreateView, UserModelViewSet, UserRegistrationViewSet, WarehouseModelViewSet
+from api.views import ProductModelViewSet, StockViewSet, TransactionCreateView, UserModelViewSet, UserRegistrationViewSet, WarehouseModelViewSet
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
 router.register('warehouses', WarehouseModelViewSet)
 router.register('products', ProductModelViewSet)
 router.register('transaction', TransactionCreateView)
+router.register('stock', StockViewSet)
 
 
 urlpatterns = [
